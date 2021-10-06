93ff6e933906614c1ffb7489102c053b17d70c9b
Walidnurudin <walidnurudin47@gmail.com>
2021-10-06 19:53:37 +0700

    fix bug redis & mysql

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -2,15 +2,10 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_DATABASE,
-
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "ticketing",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 connection.connect((err) => {
--- src/config/redis.js
+++ src/config/redis.js
@@ -1,13 +1,11 @@
 const redis = require("redis");
 require("dotenv").config();
 
-// const client = redis.createClient({
-//   host: process.env.REDIS_HOST,
-//   port: process.env.REDIS_PORT,
-//   password: process.env.REDIS_PASSWORD,
-// });
-
-const client = redis.createClient();
+const client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
 
 client.on("connect", () => {
   // eslint-disable-next-line no-console
